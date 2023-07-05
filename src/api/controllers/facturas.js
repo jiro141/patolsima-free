cfa1bd7f41b8ac081684706e1fd6a504649a6295
Maria Carrero <mariacarrerochacon267@gmail.com>
2023-07-05 19:12:53 -0400

    fix id

--- src/api/controllers/facturas.js
+++ src/api/controllers/facturas.js
@@ -2,12 +2,7 @@
 import Axios from "api/authApi";
 
 export const getFacturasList= async () => {
-<<<<<<< HEAD
     // console.log(token);
-=======
-    const token = localStorage.getItem("access");
-    Axios.defaults.headers["Authorization"] = `Bearer ${token}`;
->>>>>>> 21ee66a46830f329f7cef63d216b28f7d804a4e5
     try {
         const response = await Axios.get(`/v1/facturacion/ordenes/`)
         return response.data.results;
@@ -53,12 +48,6 @@ export const postConfirmar= async (id) => {
 }
 //nota de pago
 export const postNotaPago= async (id) => {
-<<<<<<< HEAD
-=======
-    const token = localStorage.getItem("access");
-    Axios.defaults.headers["Authorization"] = `Bearer ${token}`;
-    console.log(id);
->>>>>>> 21ee66a46830f329f7cef63d216b28f7d804a4e5
     try {
         const response = await Axios.get(`/v1/facturacion/pagos/${id}/nota_de_pago/`)
         return response;
