e793f0e74d2796328208104d1bbea6bc1dde3f94
jiro141 <91983449+jiro141@users.noreply.github.com>
2023-07-19 12:50:40 -0400

    nuevo

---
+++ src/components/widgets/Buttons/PreStation.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { Box, Button, FormLabel } from '@chakra-ui/react';
+import { BsFillArrowRightCircleFill } from 'react-icons/bs';
+import MainContext from 'context/mainContext/MainContext';
+
+export const PreStation = () => {
+  const { activeTab, setActiveTab } = useContext(MainContext);
+
+  const handleDeincrement = () => {
+    setActiveTab(activeTab - 1);
+  };
+
+  return (
+    <Box
+      display={'flex'}
+      justifyContent={'end'}
+      color='#ffff'
+      onClick={handleDeincrement}
+      cursor='pointer'
+      _hover={{ opacity: 0.8 }}
+    >
+      <FormLabel color={'black'}>Siguiente etapa</FormLabel>
+      <BsFillArrowRightCircleFill size={'25px'} color='#137797' />
+    </Box>
+
+  );
+};
